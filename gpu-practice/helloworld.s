5289ab2c8e302941cd4b3ed996c85f2e4c44df56
Melvyn Andrew <melvynandrew99@gmail.com>
2024-04-19 23:33:54 -0400

    setup psx display using the GP1 register

--- gpu-practice/helloworld.s
+++ gpu-practice/helloworld.s
@@ -22,6 +22,28 @@
 ;; Finally we can draw a flat-shaded triangle
 ; Send packets to the GP0 to draw a triangle
 
+;-------------------------------------------
+; Declare Variables
+;-------------------------------------------
+IO_BASE_ADDR equ 0x1F80   ; IO Ports Memory map base address
+GP0 equ 0x1810            ; GP0 @ $1F801810: Render data & VRAM Access
+GP1 equ 0x1814            ; GP1 @ $1F801814: Display Control and Env Setup
+
 Main:
+	lui $t0, IO_BASE_ADDR   ; t0 = I/O Port Base Address (mapped at 0x1F80)
+	li $t1, 0x00000000      ; 00 = Reset GPU
+	sw $t1, GP1($t0)        ; Writes the packet to the GP1 with the offset of $t0
+
+	li $t1, 0x03000000      ; 03 = Display Enable, 03 is the command, everything else are the params
+	sw $t1, GP1($t0)        ; send packet to GPU, the offset is whatever is in t0 + 1814 from GP1
+
+	li $t1, 0x08000001      ; 08 = Display mode (320x240, 15-bit, NTSC)
+	sw $t1, GP1($t0)
+
+	li $t1, 0x06C60260      ; 06 = Horizontal Display range 0xXXXxxx (3168-608)
+	sw $t1, GP1($t0)
+
+	li $t1, 0x07042018      ; 07 = Vertical Display Range %yyyyyyyyyyYYYYYYYYYY (264-24)
+	sw $t1, GP1($t0)
 
 .close
