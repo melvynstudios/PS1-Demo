75697b585b4b9fa4bc7dd5ed3112b5c755fe4122
Melvyn Andrew <melvynandrew99@gmail.com>
2024-04-22 23:12:06 -0400

    removed duplicate code for IO_BASE_ADDR

--- memory-practice/hello.s
+++ memory-practice/hello.s
@@ -10,49 +10,49 @@ GP0 equ 0x1810            ; GP0 @ $1F801810: Render data & VRAM Access
 GP1 equ 0x1814            ; GP1 @ $1F801814: Display Control and Env Setup
 
 Main:
-	lui $t0, IO_BASE_ADDR   ; t0 = I/O Port Base Address (mapped at 0x1F80)
+	lui $a0, IO_BASE_ADDR        ; Different from above because that is using $t0 register
 
 	;-------------------------------------------
 	; Setup Display and VRAM
 	;-------------------------------------------
 	li $t1, 0x00000000      ; 00 = Reset GPU
-	sw $t1, GP1($t0)        ; Writes the packet to the GP1 with the offset of $t0
+	sw $t1, GP1($a0)        ; Writes the packet to the GP1 with the offset of $t0
 
 	li $t1, 0x03000000      ; 03 = Display Enable, 03 is the command, everything else are the params
-	sw $t1, GP1($t0)        ; send packet to GPU, the offset is whatever is in t0 + 1814 from GP1
+	sw $t1, GP1($a0)        ; send packet to GPU, the offset is whatever is in t0 + 1814 from GP1
 
 	li $t1, 0x08000001      ; 08 = Display mode (320x240, 15-bit, NTSC)
-	sw $t1, GP1($t0)
+	sw $t1, GP1($a0)
 
 	li $t1, 0x06C60260      ; 06 = Horizontal Display range 0xXXXxxx (3168-608)
-	sw $t1, GP1($t0)
+	sw $t1, GP1($a0)
 
 	li $t1, 0x07042018      ; 07 = Vertical Display Range %yyyyyyyyyyYYYYYYYYYY (264-24)
-	sw $t1, GP1($t0)
+	sw $t1, GP1($a0)
 
 	li $t1, 0xE1000400      ; E1 = Draw Mode Settings
-	sw $t1, GP0($t0)
+	sw $t1, GP0($a0)
 
 	li $t1, 0xE3000000      ; E3 = Drawing Area TopLeft - %YYYYYYYYYYXXXXXXXXXX (10 bits for x and 10 bits for y)
-	sw $t1, GP0($t0)        ; TopLeft set at x=0, y=0
+	sw $t1, GP0($a0)        ; TopLeft set at x=0, y=0
 
 	li $t1, 0xE403BD3F      ; E4 = Drawing aread BottomRight - %YYYYYYYYYYXXXXXXXXXX (10 bits for x and 10 bits for y)
-	sw $t1, GP0($t0)        ; BottomRight set to x=319, y=239 (index starts at 0, so 320x240 is the real size)
+	sw $t1, GP0($a0)        ; BottomRight set to x=319, y=239 (index starts at 0, so 320x240 is the real size)
 
 	li $t1, 0xE5000000      ; E5 = Drawing offset - %YYYYYYYYYYXXXXXXXXXX
-	sw $t1, GP0($t0)        ; Drawing offset x=0, y=0
+	sw $t1, GP0($a0)        ; Drawing offset x=0, y=0
 
 	; ------------------------------
 	; Clear Screen and Draw background
 	; ------------------------------
 	li $t1, 0x02FF2C2E      ; 02 = Fill Rectangle in VRAM (Color Parameter: 0xBBGGRR)
-	sw $t1, GP0($t0)        ; r=46, g=44, b=255
+	sw $t1, GP0($a0)        ; r=46, g=44, b=255
 
 	li $t1, 0x00000000      ; Fill Area, Parameter: 0xYYYYXXXX - Topleft (0,0)
-	sw $t1, GP0($t0)
+	sw $t1, GP0($a0)
 
 	li $t1, 0x00EF013F      ; Fill Area, 0xHHHHWWWW
-	sw $t1, GP0($t0)
+	sw $t1, GP0($a0)
 
 	; ------------------------------
 	; Draw flat-shaded triangle
@@ -60,76 +60,71 @@ Main:
 	; ------------------------------
 	; Left Eye
 	li $t1, 0x2000FFFF         ; 20 = Flat-shaded triangle (Parameter Color: 0xBBGGRR)
-	sw $t1, GP0($t0)           ; Write GP0 Command
+	sw $t1, GP0($a0)           ; Write GP0 Command
 
 	li $t1, 0x00500064         ; Vertex 1: (Parameter 0xYyyyXxxx) (x=50,y=50)
-	sw $t1, GP0($t0)           ; Write GP0 Command
+	sw $t1, GP0($a0)           ; Write GP0 Command
 
 	li $t1, 0x003C0078         ; Vertex 2: (Parameter 0xYyyyXxxx) (x=100,y=30)
-	sw $t1, GP0($t0)           ; Write GP0 Command
+	sw $t1, GP0($a0)           ; Write GP0 Command
 
 	li $t1, 0x0050008C         ; Vertex 3: (Parameter 0xYyyyXxxx) (x=110,y=100)
-	sw $t1, GP0($t0)           ; Write GP0 Command
+	sw $t1, GP0($a0)           ; Write GP0 Command
 	; ---------------------------------------------------------------------------------
 	; Right Eye
 	li $t1, 0x2000FFFF         ; 20 = Flat-shaded triangle (Parameter Color: 0xBBGGRR)
-	sw $t1, GP0($t0)           ; Write GP0 Command
+	sw $t1, GP0($a0)           ; Write GP0 Command
 
 	li $t1, 0x005000B4         ; Vertex 1: (Parameter 0xYyyyXxxx) (x=50,y=50)
-	sw $t1, GP0($t0)           ; Write GP0 Command
+	sw $t1, GP0($a0)           ; Write GP0 Command
 
 	li $t1, 0x003C00C8         ; Vertex 2: (Parameter 0xYyyyXxxx) (x=100,y=30)
-	sw $t1, GP0($t0)           ; Write GP0 Command
+	sw $t1, GP0($a0)           ; Write GP0 Command
 
 	li $t1, 0x005000DC         ; Vertex 3: (Parameter 0xYyyyXxxx) (x=110,y=100)
-	sw $t1, GP0($t0)           ; Write GP0 Command
+	sw $t1, GP0($a0)           ; Write GP0 Command
 	; ---------------------------------------------------------------------------------
 	; Nose
 	li $t1, 0x2000FFFF         ; 20 = Flat-shaded triangle (Parameter Color: 0xBBGGRR)
-	sw $t1, GP0($t0)           ; Write GP0 Command
+	sw $t1, GP0($a0)           ; Write GP0 Command
 
 	li $t1, 0x007800A0         ; Vertex 1: (Parameter 0xYyyyXxxx) (x=160,y=120)
-	sw $t1, GP0($t0)           ; Write GP0 Command
+	sw $t1, GP0($a0)           ; Write GP0 Command
 
 	li $t1, 0x008C0096         ; Vertex 2: (Parameter 0xYyyyXxxx) (x=150,y=140)
-	sw $t1, GP0($t0)           ; Write GP0 Command
+	sw $t1, GP0($a0)           ; Write GP0 Command
 
 	li $t1, 0x008C00AA         ; Vertex 3: (Parameter 0xYyyyXxxx) (x=170,y=140)
-	sw $t1, GP0($t0)           ; Write GP0 Command
+	sw $t1, GP0($a0)           ; Write GP0 Command
 	; ---------------------------------------------------------------------------------
 	; mouth
 	li $t1, 0x2000FFFF         ; 20 = Flat-shaded triangle (Parameter Color: 0xBBGGRR)
-	sw $t1, GP0($t0)           ; Write GP0 Command
+	sw $t1, GP0($a0)           ; Write GP0 Command
 
 	li $t1, 0x00A00078         ; Vertex 1: (Parameter 0xYyyyXxxx) (x=120,y=160)
-	sw $t1, GP0($t0)           ; Write GP0 Command
+	sw $t1, GP0($a0)           ; Write GP0 Command
 
 	li $t1, 0x00A000C8         ; Vertex 2: (Parameter 0xYyyyXxxx) (x=200,y=160)
-	sw $t1, GP0($t0)           ; Write GP0 Command
+	sw $t1, GP0($a0)           ; Write GP0 Command
 
 	li $t1, 0x00B400A0         ; Vertex 3: (Parameter 0xYyyyXxxx) (x=160,y=180)
-	sw $t1, GP0($t0)           ; Write GP0 Command
+	sw $t1, GP0($a0)           ; Write GP0 Command
 	; ---------------------------------------------------------------------------------
 	; cigar
 	li $t1, 0x30283C8B         ; 30 = Gourand-shaded triangle
-	sw $t1, GP0($t0)
+	sw $t1, GP0($a0)
 	li $t1, 0x00BA006E         ; Vertex 1: 0xYyyyXxxx (x=155,y=160)
-	sw $t1, GP0($t0)
+	sw $t1, GP0($a0)
 
 	li $t1, 0x0054382B         ; Color 2 0xBBGGRR
-	sw $t1, GP0($t0)
+	sw $t1, GP0($a0)
 	li $t1, 0x00BA0082         ; Vertex 2: x=165, y=160
-	sw $t1, GP0($t0)
+	sw $t1, GP0($a0)
 
 	li $t1, 0x00302A1E         ; Color 2 0xBBGGRR
-	sw $t1, GP0($t0)
+	sw $t1, GP0($a0)
 	li $t1, 0x00A60096         ; Vertex 3: x=175, y=150
-	sw $t1, GP0($t0)
-
-	; ------------------------------------------------------------------------------------
-	; Set IO_BASE_ADDR to $a0 which is used for a global parameter
-	; ------------------------------------------------------------------------------------
-	lui $a0, IO_BASE_ADDR        ; Different from above because that is using $t0 register
+	sw $t1, GP0($a0)
 	; ------------------------------------------------------------------------------------
 	; Use Memory on the stack to call function
 	la $sp, 0x00103CF0      ; Initialize stack pointer to 0x00103CF0
