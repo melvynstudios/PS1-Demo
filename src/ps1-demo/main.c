cfce3e30747b5ef4397469819b9be8d942f5838b
Melvyn Andrew <melvynandrew99@gmail.com>
2024-04-28 23:00:01 -0400

    setup gpu to render purple background

--- src/ps1-demo/main.c
+++ src/ps1-demo/main.c
@@ -6,19 +6,67 @@
 #define VIDEO_MODE 0
 #define SCREEN_RES_X 320
 #define SCREEN_RES_Y 240
-#define SCREEN_CENTER_X (SCREEN_RES_X >> 1)
+#define SCREEN_CENTER_X (SCREEN_RES_X >> 1) // bit shifting for division, this is divided by 2
 #define SCREEN_CENTER_Y (SCREEN_RES_Y >> 1)
 
+typedef struct {
+  DRAWENV draw[2];   // Draw Buffer is the back buffer which we draw to
+  DISPENV disp[2];   // Display Buffer is the front buffer which we display
+} DoubleBuff;
+
+DoubleBuff screen;
+short currentBuff;  // 0 or 1 which contains the curren buffer.  We swap the buffers on the backend
+
 void ScreenInit(void) {
-  // TODO: set video mode
+  // Reset GPU
+  ResetGraph(0);
+
+  // Set the display area of the first buffer
+  SetDefDispEnv(&screen.disp[0], 0, 0, SCREEN_RES_X, SCREEN_RES_Y);
+  SetDefDrawEnv(&screen.draw[0], 0, 240, SCREEN_RES_X, SCREEN_RES_Y);
+
+  // Set the display area of the second buffer, the inverse of the first buffer
+  SetDefDispEnv(&screen.disp[1], 0, 240, SCREEN_RES_X, SCREEN_RES_Y);
+  SetDefDrawEnv(&screen.draw[1], 0, 0, SCREEN_RES_X, SCREEN_RES_Y);
+
+  // Set the back/drawing buffer
+  screen.draw[0].isbg = 1;
+  screen.draw[1].isbg = 1;
+
+  // Set the background clear color
+  setRGB0(&screen.draw[0], 63, 0, 127); // dark purple
+  setRGB0(&screen.draw[1], 63, 0, 127); // dark purple
+
+  // Set the current buffer
+  currentBuff = 0;
+  PutDispEnv(&screen.disp[currentBuff]);
+  PutDrawEnv(&screen.draw[currentBuff]);
+
+  // Initialize and setup the GTE geometry offsets
+  InitGeom();
+  SetGeomOffset(SCREEN_CENTER_X, SCREEN_CENTER_Y);
+  SetGeomScreen(SCREEN_CENTER_X);
+
+  // Enable Display
+  SetDispMask(1);
 }
 
 void DisplayFrame(void) {
-  // TODO: display frame
+  // Controls the vsync of the display
+  DrawSync(0);
+  VSync(0);
+
+  PutDispEnv(&screen.disp[currentBuff]);
+  PutDrawEnv(&screen.draw[currentBuff]);
+
+  // TODO: Sort objects in Ordering Table
+
+  // Swaps the buffers
+  currentBuff = !currentBuff;
 }
 
 void Setup(void) {
-  // TODO: setup
+  ScreenInit();
 }
 
 void Update(void) {
@@ -26,11 +74,14 @@ void Update(void) {
 }
 
 void Render(void) {
-  // TODO: render
+  DisplayFrame();
 }
 
 int main(void) {
-  // TODO:
-
+  Setup();
+  while (1) {
+    Update();
+    Render();
+  }
   return 0;
 }
--- src/ps1-demo/readme.md
+++ src/ps1-demo/readme.md
@@ -1,3 +1,12 @@
 # Playstation 1 Game
 
 ## Setup Dev Environment
+
+See Spinning Cube readme.md for more information. [SETUP](../../SpinningCube/readme.md)
+
+### Included Libraries
+
+stdlib is the standard library for `C`. <br />
+libgte.h is the standard library for dealing with 3d transformations, vectors, matrices and geometry. <br />
+libetc.h is the standard library for dealing with joystick input, CD-Rom. <br />
+libgpu.h is the standard library for dealing with the double-buffering, set display mode, resolution, etc. <br />
