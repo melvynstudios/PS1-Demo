4e205bfbd67075947bc4385da60c3a618f8224f2
Melvyn Andrew <melvynandrew99@gmail.com>
2024-05-13 23:17:09 -0400

    Started seperating logic into different files

---
+++ src/joypad.h
@@ -0,0 +1,11 @@
+#ifndef JOYPAD_H
+#define JOYPAD_H
+
+#define PAD1_SELECT   _PAD(0, PADselect)
+#define PAD1_START    _PAD(0, PADstart)
+#define PAD1_UP _PAD(0, PADLup)
+#define PAD1_RIGHT    _PAD(0, PADLright)
+
+
+#define PAD2_SELECT       _PAD(1, PADselect)
+#endif
