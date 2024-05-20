0a7174dd6356027fccf0929b33f5e0d0285f3856
Melvyn Andrew <melvynandrew99@gmail.com>
2024-05-19 21:16:24 -0400

    moved display code into display.c

--- src/display.c
+++ src/display.c
@@ -1,7 +1,63 @@
 #include "display.h"
+#include "globals.h"
+#include "libgpu.h"
 
 static DoubleBuffer screen;
 static u_short currentBuffer;
 
-// TODO(PHIL):
-// Implement the getters and setters for screen and for current buffer
+u_short GetCurrentBuffer() { return currentBuffer; }
+void ScreenInit(void) {
+  // Reset GPU
+  ResetGraph(0);
+
+  // Set the digital display area of the first buffer
+  SetDefDispEnv(&screen.disp[0], 0, 0, SCREEN_RES_X, SCREEN_RES_Y);
+  SetDefDrawEnv(&screen.draw[0], 0, 240, SCREEN_RES_X, SCREEN_RES_Y);
+
+  // Set the digital display area of the second buffer
+  SetDefDispEnv(&screen.disp[1], 0, 240, SCREEN_RES_X, SCREEN_RES_Y);
+  SetDefDrawEnv(&screen.draw[1], 0, 0, SCREEN_RES_X, SCREEN_RES_Y);
+
+  // Set the back/drawing buffer
+  screen.draw[0].isbg = 1;
+  screen.draw[1].isbg = 1;
+
+  // Set the background clear color
+  setRGB0(&screen.draw[0], 63, 0, 127); // dark purple
+  setRGB0(&screen.draw[1], 63, 0, 127); // dark purple
+
+  // Set the current buffer
+  currentBuffer = 0;
+  PutDispEnv(&screen.disp[currentBuffer]);
+  PutDrawEnv(&screen.draw[currentBuffer]);
+
+  // Initialize and setup the GTE geometry offsets
+  InitGeom();
+  SetGeomOffset(SCREEN_CENTER_X, SCREEN_CENTER_Y);
+  SetGeomScreen(SCREEN_CENTER_X);
+
+  // Enable Display
+  SetDispMask(1);
+}
+
+////////////////////////////////////////////////////////////
+// Draw the current frame primitives in the ordering table
+////////////////////////////////////////////////////////////
+void DisplayFrame(void) {
+  // Sync and wait for vertical blank
+  DrawSync(0);
+  VSync(0);
+
+  // Set the current display and draw buffers
+  PutDispEnv(&screen.disp[currentBuffer]);
+  PutDrawEnv(&screen.draw[currentBuffer]);
+
+  // Draw the ordering table for the current buffer
+  DrawOTag(GetOTAt(currentBuffer, OT_LENGTH - 1));
+
+  // swap the current buffer
+  currentBuffer = !currentBuffer;
+
+  // Reset next primitive pointer to the start of the primitive buffer
+  ResetNextPrim(currentBuffer);
+}
--- src/main.c
+++ src/main.c
@@ -5,13 +5,7 @@
 #include "inline_n.h"
 #include "globals.h"
 #include "joypad.h"
-
-#define VIDEO_MODE 0
-#define SCREEN_RES_X 320
-#define SCREEN_RES_Y 240
-#define SCREEN_CENTER_X (SCREEN_RES_X >> 1) // bit shifting for division, this is divided by 2
-#define SCREEN_CENTER_Y (SCREEN_RES_Y >> 1)
-#define SCREEN_Z 320
+#include "display.h"
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // Vertices and faces
